7d3d4b84033561ec275c5a92f8dc61fa0aa4437b
ErvinBehxheti <ervinbehxheti05@gmail.com>
2024-10-19 21:02:55 +0200

    since vercel loves playing games only prod builds will be commited

--- src/views/AboutView.ts
+++
@@ -1,7 +0,0 @@
-export default function AboutView() {
-  const app = document.getElementById("app") as HTMLDivElement;
-  app.innerHTML = `
-      <h1>About</h1>
-      <p>This is the about page</p>
-    `;
-}
