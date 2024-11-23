d9941201bdc62cdf0dcfdae5d0903968a3f229dd
ErvinBehxheti <ervinbehxheti05@gmail.com>
2024-11-23 17:45:24 +0100

    removed unneccesary elements and better css

--- src/views/Vectors/VectorsView.ts
+++ src/views/Vectors/VectorsView.ts
@@ -3,8 +3,8 @@ export default function VectorsView() {
 
   learningView.innerHTML = `
   <article class="text-white text-lg">
-        <section id="vector-introduction">
-        <h2 class="text-3xl pb-4 font-semibold">What are Vectors?</h2>
+        <section id="vector-introduction" class="space-y-4">
+        <h2 class="text-3xl font-semibold">What are Vectors?</h2>
         <p>
           A vector is an object that has both a magnitude and a direction.
           Geometrically, we can picture a vector as a directed line segment,
@@ -17,23 +17,20 @@ export default function VectorsView() {
           alt="vector"
           width="449"
           height="221"
-          class="place-self-center pt-10"
+          class="mx-auto"
         />
-        <p class="pt-5">
+        <p class="">
           Two vectors are the same if they have the same magnitude and
           direction. This means that if we take a vector and translate it to a
           new position (without rotating it), then the vector we obtain at the
           end of this process is the same vector we had in the beginning.
         </p>
-        <br />
-        <br />
         <p>
           Two examples of vectors are those that represent force and velocity.
           Both force and velocity are in a particular direction. The magnitude
           of the vector would indicate the strength of the force or the speed
           associated with the velocity.
         </p>
-        <br />
         <p>
           To denote vectors we add a small arrow on top the symbol that
           represents the vector like this: \\(\\vec{a}\\). When we want to refer
@@ -43,8 +40,8 @@ export default function VectorsView() {
         </p>
       </section>
 
-    <section id="vector-addition" class="pt-14">
-      <h2 class="text-2xl pb-4 font-semibold">Operations on Vectors</h2>
+    <section id="vector-addition" class="pt-14 space-y-4">
+      <h2 class="text-2xl font-semibold">Operations on Vectors</h2>
 
       <p>
         We can define a number of operations on vectors geometrically without
@@ -52,10 +49,10 @@ export default function VectorsView() {
         <strong>addition</strong>, <strong>subtraction</strong>, and
         <strong>multiplication</strong> by a scalar. On separate pages, we
         discuss two different ways to multiply two vectors together:
-        <u><a href="/learn/vectors/dot-product" data-link class="text-[#2bbc8a] font-semibold">the dot product</a></u> and <u><a href="/learn/vectors/cross-product" data-link class="text-[#2bbc8a] font-semibold">the cross product</a></u>.
+        <u><a href="/learn/vectors/dot-product" data-link class="text-[#2bbc8a] font-semibold">the dot product</a></u>.
       </p>
-      <br />
-      <h2 class="text-2xl pb-4 font-semibold">Addition of Vectors</h2>
+
+      <h2 class="text-2xl font-semibold pt-4">Addition of Vectors</h2>
       <p>
         Given two vectors \\(\\vec{a}\\) and \\(\\vec{b}\\) , we form their sum
         \\(\\vec{a}\\)+\\(\\vec{b}\\) , as follows. We translate the vector
@@ -68,7 +65,7 @@ export default function VectorsView() {
         src="https://mathinsight.org/media/image/image/vector_a_plus_b.png"
         width="530"
         height="152"
-        class="place-self-center py-20"
+        class="mx-auto"
       />
 
       <p>
@@ -84,13 +81,12 @@ export default function VectorsView() {
         relative to the ground.
       </p>
 
-      <br />
       <p>Addition of vectors satisfies two important properties.</p>
 
-      <div class="pl-10 pt-5">
+      <div class="pl-10">
         1. The commutative law, which states the order of addition doesn't
         matter:
-        <p class="text-center py-5">
+        <p class="text-center">
           \\(\\vec{a}\\) + \\(\\vec{b}\\) = \\(\\vec{b}\\) + \\(\\vec{a}\\)
         </p>
         <p>
@@ -103,24 +99,24 @@ export default function VectorsView() {
         <img
           src="https://mathinsight.org/media/image/image/vector_parallelogram_law.png"
           alt="CommutativeLawVector"
-          class="place-self-center py-10"
+          class="mx-auto"
           width="530"
           height="263"
         />
       </div>
 
-      <div class="pl-10 pt-5">
+      <div class="pl-10">
         2. The associative law, which states that the sum of three vectors does
         not depend on which pair of vectors is added first:
-        <p class="text-center py-5">
+        <p class="text-center">
           (\\(\\vec{a}\\) + \\(\\vec{b}\\)) + \\(\\vec{c}\\) = \\(\\vec{a}\\) +
           (\\(\\vec{b}\\) + \\(\\vec{c}\\))
         </p>
       </div>
     </section>
 
-    <section id="vector-subtraction" class="pt-14">
-      <h2 class="text-2xl pb-4 font-semibold">Subtracting Vectors</h2>
+    <section id="vector-subtraction" class="space-y-4">
+      <h2 class="text-2xl font-semibold">Subtracting Vectors</h2>
       <p>
         Before we define subtraction, we define the vector -\\(\\vec{a}\\) ,
         which is the opposite of \\(\\vec{a}\\) . The vector −\\(\\vec{a}\\) is
@@ -132,15 +128,12 @@ export default function VectorsView() {
         alt="inverse-vector"
         width="565"
         height="219"
-        class="place-self-center py-10"
+        class="mx-auto"
       />
-      <br />
       <p>We define subtraction as addition with the opposite of a vector:</p>
-      <br />
       <p class="text-center">
         \\(\\vec{b}\\) − \\(\\vec{a}\\) = \\(\\vec{b}\\) + (−\\(\\vec{a}\\)).
       </p>
-      <br />
       <p>
         This is equivalent to turning vector \\(\\vec{a}\\) around in the
         applying the above rules for addition. Can you see how the vector
@@ -152,14 +145,14 @@ export default function VectorsView() {
       <img
         src="https://mathinsight.org/media/image/image/vector_b_minus_a.png"
         alt="Vector-subtraction"
-        class="place-self-center py-10"
+        class="mx-auto"
         width="543"
         height="134"
       />
     </section>
 
-    <section id="vector-multiplication" class="pt-14">
-      <h2 class="text-2xl pb-4 font-semibold">Scalar Multiplications</h2>
+    <section id="vector-multiplication" class="space-y-4">
+      <h2 class="text-2xl font-semibold">Scalar Multiplications</h2>
       <p>
         Given a vector \\(\\vec{a}\\) and a real number (scalar) λ , we can form
         the vector λ\\(\\vec{a}\\) as follows. If λ is positive, then
@@ -168,7 +161,6 @@ export default function VectorsView() {
         \\(\\vec{a}\\) . In this case, multiplication by λ simply stretches (if
         λ>1 ) or compresses (if 0 &lt; λ &lt; 1) the vector \\(\\vec{a}\\) .
       </p>
-      <br />
       <p>
         If, on the other hand, λ is negative, then we have to take the opposite
         of \\(\\vec{a}\\) before stretching or compressing it. In other words,
@@ -178,12 +170,10 @@ export default function VectorsView() {
         λ\\(\\vec{a}\\) is |λ| times the magnitude of \\(\\vec{a}\\) :
         ∥λ\\(\\vec{a}\\)∥=|λ|∥\\(\\vec{a}\\)∥ .
       </p>
-      <br />
       <p>
         Scalar multiplications satisfies many of the same properties as the
         usual multiplication.
       </p>
-      <br />
       <div class="pl-10">
         <li>
           1.
@@ -200,21 +190,17 @@ export default function VectorsView() {
         <li>4. (−1)\\(\\vec{a}\\)=−\\(\\vec{a}\\)</li>
         <li>5. 0\\(\\vec{a}\\)=0</li>
       </div>
-      <br />
       <p>
         In the last formula, the zero on the left is the number 0, while the
         zero on the right is the vector 0 , which is the unique vector whose
         length is zero.
       </p>
-      <br />
       <p>
         If a=λb for some scalar λ , then we say that the vectors \\(\\vec{a}\\)
         and \\(\\vec{b}\\) are parallel. If λ is negative, some people say that
         \\(\\vec{a}\\) and \\(\\vec{b}\\) are anti-parallel, but we will not use
         that language.
       </p>
-      <br />
-      <br />
       <p>
         We were able to describe <strong>vectors</strong>,
         <strong>vector addition</strong>, <strong>vector subtraction</strong>,
@@ -231,33 +217,33 @@ export default function VectorsView() {
         in the plane and in three-dimensional space.
       </p>
     </section>
-    <section id="article-vector-application" class="pt-14">
+    <section id="article-vector-application" class="space-y-4 pt-4">
       <h2 class="text-2xl font-semibold">Application of Vectors in Computer Science and Physics</h2>
-      <br />
-      <div>
+
+      <div class="space-y-4">
         <h1 class="font-semibold text-xl">Computer Science</h1>
-        <div class="pl-4 pt-4">
+        <div class="pl-4">
           <p class="font-medium">1. Graphics and Game Development:</p>
           <div class="pl-8">
             <li>Vectors are used to represent positions, directions, and movements in 2D/3D graphics.</li>
             <li>They help simulate realistic motion, lighting, and transformations (scaling, rotation, translation).</li>
           </div>
         </div>
-        <div class="pl-4 pt-4">
+        <div class="pl-4">
           <p class="font-medium">2. Machine Learning and AI:</p>
           <div class="pl-8">
             <li>Feature vectors represent data points in high-dimensional spaces.</li>
             <li>Used in algorithms like k-means clustering, support vector machines, and neural networks.</li>
           </div>
         </div>
-        <div class="pl-4 pt-4">
+        <div class="pl-4">
           <p class="font-medium">3. Robotics and Navigation:</p>
           <div class="pl-8">
             <li>Vectors represent paths, velocities, and forces for autonomous movement.</li>
             <li>Help calculate the shortest path or trajectory.</li>
           </div>
         </div>
-        <div class="pl-4 pt-4">
+        <div class="pl-4">
           <p class="font-medium">4. Physics Simulations:</p>
           <div class="pl-8">
             <li>Simulate natural phenomena (e.g., collisions, fluid dynamics) in computer programs</li>
@@ -265,38 +251,37 @@ export default function VectorsView() {
           </div>
         </div>
       </div>
-      <div>
-        <h1 class="font-semibold text-xl pt-4">Physics</h1>
-        <div class="pl-4 pt-4">
+      <div class="space-y-4">
+        <h1 class="font-semibold text-xl">Physics</h1>
+        <div class="pl-4">
           <p class="font-medium">1. Mechanics:</p>
           <div class="pl-8">
             <li>Vectors describe motion (displacement, velocity, acceleration) and forces (e.g., gravity, friction).</li>
             <li>Essential for solving problems involving projectile motion and Newton's laws.</li>
           </div>
         </div>
-        <div class="pl-4 pt-4">
+        <div class="pl-4">
           <p class="font-medium">2. Electromagnetism:</p>
           <div class="pl-8">
             <li>Electric and magnetic fields are vector fields.</li>
             <li>Vectors describe forces on charges and the direction of current flow.</li>
           </div>
         </div>
-        <div class="pl-4 pt-4">
+        <div class="pl-4">
           <p class="font-medium">3. Fluid Dynamics:</p>
           <div class="pl-8">
             <li>Velocity and acceleration of fluid particles are represented as vectors.</li>
             <li>Helps model airflow, ocean currents, and weather patterns.</li>
           </div>
         </div>
-        <div class="pl-4 pt-4">
+        <div class="pl-4">
           <p class="font-medium">4. Astronomy and Space Science:</p>
           <div class="pl-8">
             <li>Vectors calculate trajectories, orbital mechanics, and forces acting on celestial bodies.</li>
           </div>
         </div>
       </div>
-      <br />
-      <h1 class="font-semibold text-xl py-4">Why should I learn Vectors?</h1>
+      <h1 class="font-semibold text-xl">Why should I learn Vectors?</h1>
       <p>Vectors are the backbone of many fields, including computer graphics, machine learning, physics, robotics, and engineering.They provide a systematic way to break down and analyze problems involving directions, magnitudes, and projections.Understanding vectors helps you design simulations, optimize machine learning models, and solve physics problems.</p>
     </section>
   </article>
